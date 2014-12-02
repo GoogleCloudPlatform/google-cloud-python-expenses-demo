03393af5b124a6982a76c3a7c506db9d0c5f49b6
Tres Seaver <tseaver@palladion.com>
2014-12-01 19:16:10 -0500

    Add view showing employees.

--- gcloud_expenses/views.py
+++ gcloud_expenses/views.py
@@ -1,14 +1,22 @@
 from pyramid.renderers import get_renderer
 from pyramid.view import view_config
 
+from . import list_employees
 
 def get_main_template(request):
     main_template = get_renderer('templates/main.pt')
     return main_template.implementation()
 
+
 @view_config(route_name='home', renderer='templates/home.pt')
-def my_view(request):
+def home_page(request):
     return {}
+
+
+@view_config(route_name='employees', renderer='templates/employees.pt')
+def employees(request):
+    return {'employees': list_employees()}
+
 
 def includeme(config):
     config.add_request_method(callable=get_main_template,
--- gcloud_expenses/webapp.py
+++ gcloud_expenses/webapp.py
@@ -9,5 +9,6 @@ def main(global_config, **settings):
     config.include('.views')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('employees', '/employees/')
     config.scan()
     return config.make_wsgi_app()
